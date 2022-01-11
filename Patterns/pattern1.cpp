baf0e51bbafab905e2dbc288314df658ed24ece6
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2022-01-11 21:57:41 +0530

    Update pattern1.cpp

--- Patterns/pattern1.cpp
+++ Patterns/pattern1.cpp
@@ -1,1 +1,16 @@
-test
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    for(int i=0;i<5;i++){
+
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+}
