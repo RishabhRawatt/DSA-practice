13cc5aecd5ea9c2167604a462b34d8678a64ad63
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2022-01-11 22:03:55 +0530

    Create pattern2.cpp

---
+++ Patterns/pattern2.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    for(int i=0;i<5;i++){
+
+        for(int k=5;k>i;k--){
+            cout<<" ";
+        }
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+}
+
+
+
+     *
+    **
+   ***
+  ****
+ *****
